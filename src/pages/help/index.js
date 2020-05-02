c602f1d75a1f3cb8b368a6df4d499f8426cdf51d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-02 17:47:13 +0100

    Add css and static content to help page

--- src/pages/help/index.js
+++ src/pages/help/index.js
@@ -1,1 +1,6 @@
+/* Global styles import */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "../../common.css";
+
 console.log("Entry point for help page");
