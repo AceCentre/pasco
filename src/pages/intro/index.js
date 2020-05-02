b810a09408451c0b943088ccc9099fb2af57b301
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-02 14:26:22 +0100

    Fix next arrow

--- src/pages/intro/index.js
+++ src/pages/intro/index.js
@@ -1,14 +1,15 @@
-/** Global style imports */
+/* Global style imports */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "../../main.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-/** Asset import */
+/* Asset import */
 import introVideo from "./assets/intro.mp4";
 import introScreenshot from "./assets/intro.png";
 import introTrack from "./assets/intro.vtt";
 
+/* Load video assets */
 const videoEl = document.getElementById("intro-video");
 const trackEl = document.getElementById("intro-video-track");
 const sourceEl = document.getElementById("intro-video-source");
@@ -16,23 +17,16 @@ const sourceEl = document.getElementById("intro-video-source");
 videoEl.poster = introScreenshot;
 trackEl.src = introTrack;
 sourceEl.src = introVideo;
-
 videoEl.load();
 
-console.log({
-  introScreenshot,
-  introVideo,
-  introTrack,
-  videoEl,
-  trackEl,
-  sourceEl,
-});
-
-document.querySelector("#intro-video").addEventListener(
+/* Switch skip and start buttons on video end */
+const skipButton = document.getElementById("skip-btn");
+const startBtn = document.getElementById("start-btn");
+videoEl.addEventListener(
   "ended",
-  function() {
-    document.querySelector("#skip-btn").classList.add("hidden");
-    document.querySelector("#start-btn").classList.remove("hidden");
+  () => {
+    skipButton.classList.add("hidden");
+    startBtn.classList.remove("hidden");
   },
   false
 );
