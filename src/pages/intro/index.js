c4c405cd784310efe7c666e8534d433604022328
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-02 14:19:14 +0100

    Setup and load assets

--- src/pages/intro/index.js
+++ src/pages/intro/index.js
@@ -1,8 +1,32 @@
-/** global style imports */
+/** Global style imports */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "../../main.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+
+/** Asset import */
+import introVideo from "./assets/intro.mp4";
+import introScreenshot from "./assets/intro.png";
+import introTrack from "./assets/intro.vtt";
+
+const videoEl = document.getElementById("intro-video");
+const trackEl = document.getElementById("intro-video-track");
+const sourceEl = document.getElementById("intro-video-source");
+
+videoEl.poster = introScreenshot;
+trackEl.src = introTrack;
+sourceEl.src = introVideo;
+
+videoEl.load();
+
+console.log({
+  introScreenshot,
+  introVideo,
+  introTrack,
+  videoEl,
+  trackEl,
+  sourceEl,
+});
 
 document.querySelector("#intro-video").addEventListener(
   "ended",
