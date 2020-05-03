1a83e705e2974340bde60b565d7c1054f48d1bfb
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-03 15:53:06 +0100

    Hide and show go top button on scroll

--- src/pages/help/index.js
+++ src/pages/help/index.js
@@ -3,4 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "../../common.css";
 
+import showOrHideGoTopLink from "./show-or-hide-go-top-link";
+
+// const HELP_FILES = {
+//   en: "help.html",
+//   "es-ES": "help/es-ES.html",
+// };
+
+document.addEventListener("scroll", showOrHideGoTopLink);
+
 console.log("Entry point for help page");
