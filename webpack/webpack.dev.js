e90abce107d356631f417b6467a72946427f48fd
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-09 16:13:35 +0100

    Added a landing page in dev mode and only server webpack stuff in dev mode

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,26 @@
 const commonConfig = require("./webpack.common");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const relativeToRoot = (pathName) => path.resolve(__dirname, "../", pathName);
 
 const config = {
   ...commonConfig,
+  plugins: [
+    ...commonConfig.plugins,
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: relativeToRoot("src/pages/landing-page/index.html"),
+      chunks: [],
+    }),
+  ],
+  output: {
+    ...commonConfig.output,
+    path: relativeToRoot("dist"),
+  },
   mode: "development",
   devServer: {
-    contentBase: path.join(__dirname, "../html"),
+    contentBase: relativeToRoot("dist"),
     port: 9000,
     open: true,
   },
