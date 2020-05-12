a3d6768d7d5311bd46ae27c2f00cea4b85233af5
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-12 20:19:13 +0100

    Remove comments

--- src/initializeCordova.js
+++ src/initializeCordova.js
@@ -1,5 +1,5 @@
 import _ from "underscore";
-import { resolveLocalFileSystemUrl } from "./fs";
+import { resolveLocalFileSystemUrl, readFile } from "./fs";
 
 const REPLACE_FILE_KEYS = ["default_config", "default_trees_info_fn"];
 
@@ -10,62 +10,15 @@ const intializeCordova = async () => {
 
     try {
       await resolveLocalFileSystemUrl(newPath);
-      window[currentKey];
+      window[currentKey] = newPath;
     } catch (err) {
-      // read_file(path)
-      //   .catch(function(err) {
-      //     if (err.caused_by && err.caused_by.code == 1) {
-      //       // not found
-      //       return { __notfound: true };
-      //     } else {
-      //       throw err;
-      //     }
-      //   })
-      //   .then(function(data) {
-      //     if (!data.__notfound) {
-      //       return write_file(newpath, data);
-      //     }
-      //   });
+      const file = readFile(path);
+      write_file(newPath, file);
     }
-
-    // resolveLocalFileSystemUrl
   });
 
   return Promise.all(promises);
 };
-
-//   _.each(REPLACE_FILE_KEYS, function(key) {
-//     const path = window[key];
-//     const newpath = window.cordova_user_dir_prefix + window[key];
-
-//     promises.push(
-//       new Promise(function(resolve, reject) {
-//         window.resolveLocalFileSystemURL(newpath, resolve, continue_proc);
-//         function continue_proc(err) {
-//           // if not found, write it, if it exists
-//           read_file(path)
-//             .catch(function(err) {
-//               if (err.caused_by && err.caused_by.code == 1) {
-//                 // not found
-//                 return { __notfound: true };
-//               } else {
-//                 throw err;
-//               }
-//             })
-//             .then(function(data) {
-//               if (!data.__notfound) {
-//                 return write_file(newpath, data);
-//               }
-//             })
-//             .then(resolve, reject);
-//         }
-//       }).then(function() {
-//         window[key] = newpath;
-//       })
-//     );
-//   });
-//   return Promise.all(promises);
-// };
 
 const initializeApp = async () => {
   if (!window.cordova) {
