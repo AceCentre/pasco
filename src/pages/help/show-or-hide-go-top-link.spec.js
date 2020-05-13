6e897a5b76aae9b665d20386b26a58de8196685b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-13 21:36:55 +0100

    Improve tests with 'testing' library

--- src/pages/help/show-or-hide-go-top-link.spec.js
+++ src/pages/help/show-or-hide-go-top-link.spec.js
@@ -1,63 +1,49 @@
 import showOrHideGoToTopLink from "./show-or-hide-go-top-link";
+import { getByText } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("showOrHideGoToTopLink", () => {
   it("Throws an error if it cant find the go to top link", () => {
-    document.body.innerHTML = `
-        <div>
-        </div>
-    `;
-
     expect(showOrHideGoToTopLink).toThrow('Couldnt find "#move-top-link"');
   });
 
   it("Adds hidden class if scroll position is under 500", () => {
-    document.body.innerHTML = `
-        <div>
-            <a id="move-top-link">Go to top link</a>
-        </div>
-    `;
+    // Arrange
+    document.body.innerHTML = '<a id="move-top-link">Go to top link</a>';
     window.scrollY = 1;
 
+    // Act
     showOrHideGoToTopLink();
 
-    expect(document.body.innerHTML).toEqual(`
-        <div>
-            <a id="move-top-link" class="hidden">Go to top link</a>
-        </div>
-    `);
+    // Assert
+    const link = getByText(document, "Go to top link");
+    expect(link).toHaveClass("hidden");
   });
 
   it("Removes hidden class if scroll position is under 500", () => {
-    document.body.innerHTML = `
-        <div>
-            <a id="move-top-link" class="hidden">Go to top link</a>
-        </div>
-    `;
+    // Arrange
+    document.body.innerHTML =
+      '<a id="move-top-link" class="hidden">Go to top link</a>';
     window.scrollY = 501;
 
+    // Act
     showOrHideGoToTopLink();
 
-    expect(document.body.innerHTML).toEqual(`
-        <div>
-            <a id="move-top-link" class="">Go to top link</a>
-        </div>
-    `);
+    // Assert
+    const link = getByText(document, "Go to top link");
+    expect(link).not.toHaveClass("hidden");
   });
 
   it("Adds hidden class if scroll position isnt defined", () => {
-    document.body.innerHTML = `
-        <div>
-            <a id="move-top-link">Go to top link</a>
-        </div>
-    `;
-    window = {};
+    // Arrange
+    document.body.innerHTML = '<a id="move-top-link">Go to top link</a>';
+    window.scrollY = undefined;
 
+    // Act
     showOrHideGoToTopLink();
 
-    expect(document.body.innerHTML).toEqual(`
-        <div>
-            <a id="move-top-link" class="hidden">Go to top link</a>
-        </div>
-    `);
+    // Assert
+    const link = getByText(document, "Go to top link");
+    expect(link).toHaveClass("hidden");
   });
 });
