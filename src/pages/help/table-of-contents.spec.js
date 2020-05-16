2f5dae504c30d282aef7790e2b74f84539d408d7
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-16 15:31:55 +0100

    Added more table of contents tests

--- src/pages/help/table-of-contents.spec.js
+++ src/pages/help/table-of-contents.spec.js
@@ -3,6 +3,10 @@ import { screen } from "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
 
 describe("generateTableOfContents", () => {
+  afterEach(() => {
+    document.body.innerHTML = "";
+  });
+
   describe("given an empty list", () => {
     it("returns an empty ordered list", () => {
       const tableOfContents = generateTableOfContents([]);
@@ -15,9 +19,19 @@ describe("generateTableOfContents", () => {
     });
   });
 
-  //   describe("given a list of heading items", () => {
-  //     it("returns an ordered list with links", () => {
-  //       // const
-  //     });
-  //   });
+  describe("given a list of heading items", () => {
+    it("returns an ordered list with 3 items", () => {
+      const tableOfContents = generateTableOfContents([
+        { heading: "HEADING ONE", anchorHref: "#HEADING_ONE" },
+        { heading: "HEADING TWO", anchorHref: "#HEADING_TWO" },
+        { heading: "HEADING THREE", anchorHref: "#HEADING_THREE" },
+      ]);
+      document.body.append(tableOfContents);
+
+      const list = screen.getByRole("list");
+
+      expect(list.tagName).toStrictEqual("OL");
+      expect(list.childElementCount).toEqual(3);
+    });
+  });
 });
