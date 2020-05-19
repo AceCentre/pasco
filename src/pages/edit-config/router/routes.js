7ddd7d01b615814b18604c473eeb50f733da3805
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-05-19 20:43:10 +0100

    Alter routing so it matches the previous routing

--- src/pages/edit-config/router/routes.js
+++ src/pages/edit-config/router/routes.js
@@ -1,14 +1,55 @@
 const setRouteHandler = (route) => () => {
   const ListItem = document.querySelector(`a[data-name='${route}']`);
   ListItem.classList.add("active");
+
+  const PageSection = document.querySelector(
+    `.page-sect[data-name='${route}']`
+  );
+  PageSection.classList.add("active");
+
+  const MobilePageHeading = document.querySelector(`h2[data-name='${route}']`);
+  MobilePageHeading.classList.add("active");
 };
 
 const unsetRouteHandler = (route) => () => {
   const ListItem = document.querySelector(`a[data-name='${route}']`);
   ListItem.classList.remove("active");
+
+  const PageSection = document.querySelector(
+    `.page-sect[data-name='${route}']`
+  );
+  PageSection.classList.remove("active");
+
+  const MobilePageHeading = document.querySelector(`h2[data-name='${route}']`);
+  MobilePageHeading.classList.remove("active");
 };
 
 const routes = [
+  {
+    path: "",
+    title: "pasco - Config",
+    setRoute: () => {
+      const SelectorList = document.getElementById("page-selector-list");
+      SelectorList.classList.remove("has-active");
+
+      const SettingsTitle = document.querySelector("h1.main-head");
+
+      SettingsTitle.classList.remove("has-active");
+
+      const BackButton = document.querySelector("a.back-btn");
+      BackButton.classList.remove("has-active");
+    },
+    unsetRoute: () => {
+      const SelectorList = document.getElementById("page-selector-list");
+      SelectorList.classList.add("has-active");
+
+      const SettingsTitle = document.querySelector("h1.main-head");
+      SettingsTitle.classList.add("has-active");
+
+      const BackButton = document.querySelector("a.back-btn");
+      BackButton.classList.add("has-active");
+    },
+  },
   {
     path: "access",
     title: "pasco - Config | Access",
@@ -42,8 +83,7 @@ const routes = [
 ];
 
 export const defaultRoute = {
-  path: "access",
-  title: "pasco - Config | Access",
+  path: "",
 };
 
 export default routes;
