f92b552ea9a4dddf4460b7b3dbed37d59852bcf1
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-06-08 15:09:35 +0100

    Pressing add adds key to list

--- src/pages/edit-config/key-selection.js
+++ src/pages/edit-config/key-selection.js
@@ -1,7 +1,8 @@
-const KEY_TEMPLATE = ({ keyCode, keyIndex, keyLabel }) => `
+// prettier-ignore
+const KEY_TEMPLATE = ({ keyCode='', keyIndex='', keyLabel='', isWaiting = false }) => `
   <div class="key-wrp" data-key="${keyCode}" data-id="${keyIndex}">
     <div class="key-btn-wrp">
-        <button type="button" class="key-btn btn btn-default" oncontextmenu="return false">
+        <button type="button" class="key-btn btn btn-default ${isWaiting ? "is-waiting" : ""}" oncontextmenu="return false">
           <span class="key-text">${keyLabel}</span><span class="waiting-text" x-l10n="">Waiting for key event</span>
         </button>
       </div>
@@ -13,17 +14,29 @@ const KEY_TEMPLATE = ({ keyCode, keyIndex, keyLabel }) => `
     </div>
   </div>`;
 
+const stringToDomNode = (htmlString) => {
+  const div = document.createElement("div");
+  div.innerHTML = htmlString.trim();
+  return div.firstChild;
+};
+
 const initKeySelection = (action, modal, initialKeys = []) => {
   const Button = document.querySelector(`button[data-action="${action}"]`);
   const KeyListContainer = document.getElementById("configure-action-key-list");
+  const AddKeyButton = document.getElementById("configure-action-key-add-btn");
 
-  if (!Button || !KeyListContainer)
+  if (!Button || !KeyListContainer || !AddKeyButton)
     throw new Error(
       "Couldnt find everything needed for key selection in the DOM"
     );
 
   Button.onclick = () => {
     modal.open();
+
+    AddKeyButton.onclick = () => {
+      const NewKey = stringToDomNode(KEY_TEMPLATE({ isWaiting: true }));
+      KeyListContainer.append(NewKey);
+    };
 
     KeyListContainer.innerHTML = "";
 
